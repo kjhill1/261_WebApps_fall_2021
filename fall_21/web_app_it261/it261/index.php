ae5e46d397825cbdb2016bf254031eaec8af9111
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2021-10-10 10:04:20 -0700

    2nd commit
    1st was not committed but these are basically the same. Footer was moved.

---
+++ fall_21/web_app_it261/it261/index.php
@@ -0,0 +1,104 @@
+<!-- PORTAL PAGE -->
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0" >
+
+<link href="css/styles_portal.css" type="text/css" rel="stylesheet">
+
+<title>My Portal Page</title>
+<!-- this is from the HTML website page css is same as website page -->
+
+</head>
+
+<header>
+    <div class="header-inner">
+        <!-- <a href="index.html"> -->
+        <img id="logo" src="images/phplogo.png" alt="logo">
+
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Daily</a></li>
+            <li><a href="">People</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Gallery</a></li>
+
+        </ul>
+   
+    </nav>
+    </div> <!-- closes header-inner -->
+    
+</header>
+
+<div id="wrapper">
+
+<!-- <div id="hero">test hero
+
+</div> closes hero -->
+    <main>
+    <h2>This is my WONDERFUL Portal Page and a little about me!</h2>
+
+    <p>Wouldn’t the obligatory “About Me” that is shameless self-promotion be so much more interesting if there was a geeky joke right at top…of course! Um, YEAH! How many programmers does it take to change a light bulb???</p>
+    <p>Now if you want to know how many, you will have to read about me!</p>
+
+    <h3>Me</h3>
+
+    <p>I love to design but I also love to code! Watching a concept realized into an experience is almost rewarding as popping bubble wrap</p>
+
+    <p>I have combined my background in art, design, marketing, and client based project management with tech to become a UX/UI designer. Through my experience, I can design and build protosites, as well as fully functional websites. I have a clear understanding and the abilities to work through the full formal UX/UI Design Process.</p>
+    <p>And I like cats, soapmaking, traveling and food.</p>
+
+    <p>So, good Job, you made it to the end and your reward: None…..It's a hardware problem of course!</p>
+
+</main>
+   
+<aside>
+    
+    <h3>Weekly Excersizes</h3>
+
+            <ul>
+                <li><a href=""><li>Site 1</a></li>
+            <ul>
+
+        <h3>Week #2:</h3>
+
+            <ul>
+                <li><a href=""><li>Variables</a></li>
+                <li><a href="">Currency-Logic</a></li>
+                <li><a href="">Currency </a></li>
+                <li><a href="">Heredoc</a></li>
+            <ul>
+                
+</aside>
+<!-- 
+<footer>
+    <ul>
+        <li>Copyright &copy; 2021</li>
+        <li>All Rights Reserved</li>
+        <li><a href="">Terms of Use</a></li>
+        <li><a href="">Seven Sites Designs</a></li>
+        <li><a href="">Validate HTML</a></li>
+        <li><a href="">Validate css</a></li>
+    </ul>
+
+</footer> -->
+
+ </div> <!--closes wrapper -->
+
+ <footer>
+    <ul>
+        <li>Copyright &copy; 2021</li>
+        <li>All Rights Reserved</li>
+        <li><a href="">Terms of Use</a></li>
+        <li><a href="">Seven Sites Designs</a></li>
+        <li><a href="">Validate HTML</a></li>
+        <li><a href="">Validate css</a></li>
+    </ul>
+
+</footer>
+
+<body>
+</body>
+</html>
