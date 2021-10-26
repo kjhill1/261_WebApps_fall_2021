3eb538c96ba4224f8632347e0f8c847f1de779d4
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2021-10-26 09:46:58 -0700

    HW 4 ToubleShooting

--- troubleshoot_wk4/hw_wk4_troubleshoot.php
+++ troubleshoot_wk4/hw_wk4_troubleshoot.php
@@ -6,19 +6,15 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
-<style>
-/* color:red for answer */
-    
-</style>
 
+<!-- *****I USED AN EXTERNAL STYLESHEET***** -->
+<link href="css/style.css" type="text/css" rel="stylesheet">
 
 <title>Adder Form - Corrected</title>
 
 </head>
-
 <body>
     <h1>Adder.php, one funny assignment</h1>
-
 
     <form action="" method="post">
 
@@ -30,62 +26,58 @@
             <input type="text" name="num2">
 
             <input type="submit" value="Add Em!!"> 
+
+            <p><a href="">Reset</a></p>
         </fieldset>
 
     </form>
-<!-- 
-
-    ********START PHP********* -->
+<!-- ********START PHP********* -->
 
     <?php
-    if(isset($_POST['num1']
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+    if(isset($_POST['num1'],
             $_POST['num2'])){
 
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
 
-                // *****CHECK THESE - JUST COPIED AND PASTED error code*****
-                $num1 = $_POST['num1'];
-                $num2 = $_POST['num2'];
-                $myTotal = $num1 + $num2; 
+    $myTotal = $num1 + $num2; 
 
+    if(empty($num1 && $num2)) {
+                echo 'Please Enter a Value';
+                } else {
+                echo '<div class="box">
+                <h2>You added '.$num1.' & '.$num2.'!</h2>
+                <p>Your answer is '.$myTotal.'</p>
+                
+                </div>';
+                }
             }
-
-
+            }    
+            
 
 ?>
+
+    <!-- ERRORS
+-Line 1, Moved to 34=lineCAnnot use Echo if PHP is before doctype - PHP nested
+-Line 3 = Doc type was not called out - Doctype Added
+-Line 11 & 13 =Stylesheet/CSS was not above Title
+-Line 11 -color:red was not assigned to an element or put in the proper 
+-Line 17 = form action was <\form action=""> was not open and no closing tag in doc
+-Line 17 - no form method stated
+-Line 22 & 23 = No opening Label Tags
+-line 44 Num2 was capitalize, not constant in capitalization of num/Num should not be capitalized if variable is called as lowercase, need to use lowercase 
+-Line 23 = num vs NUM (NUM is not consistent)
+-Line 28 = double quotes missing around "Add Em at the end
+-Line 34 = PHP moved to be nested in the html
+Line 41 = 53 NO )) at the end of vars being called out
+Line 40 = 21 Curly brackets not in right place need to be around vars being called
+Line 40  = variable in if(isset should have commas not semi colons.
+Line 52 = no br before x<h2>You added'.$num1.' & '.$num2.'!</h2> -->
+
 </body>
 </html>
 
 
-
-    <!-- ERRORS
--form action was <\form action=""> was not open and no closing tag in doc, no method stated
--Doc type was not called out - Doctype Added
--CAnnot use Echo if PHP is before doctype - PHP nested
--PHP moved to be nested in the html
- -Num2 not constant in capitalization of num/Num should not be capitalized if variable is called as lowercase, need to use lowercase 
- -Closing HTML Tag has punctuation errors
-
- -style was not in properly in doc
- -color:red was not assigned to an element or put in the proper 
- -label tags are were either missing or non-existent 
--double quotes missing around "Add Em -->
-<!-- if.....['num1'])) NO )) at the end  here after called -->
-<!-- Curly brackets not in right place need to be around vars being called -->
-
-
-
-// START MESSED UP CODE
-//adder-wrong.php
-
-if(isset($_POST['num1']))
-{
-$num1 = $_POST['num1'];
-$num2 = $_POST['num2'];
-$myTotal = $num1 + $num2; {
-echo '<h2>You added ". $num1 ." and .$num2. '"';
-echo ' <p> and the answer is <br> $myTotal ."!"</p>;
-echo'<p><a href="">Reset page</a>'
-}
-
-
-<!-- minus sign in front of equal sign -= --> 
