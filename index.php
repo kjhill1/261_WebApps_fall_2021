d305d9f5c0f85de5ac8700ed210f4cfe719965ec
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2021-11-29 08:33:15 -0800

    Login Page issses
    Login page no giving error messages or routing to the index page.

--- index.php
+++ index.php
@@ -1,22 +1,56 @@
 <?php
+
+// home page of wonderful website?????
+
+// after creating this page, will  have to login as a user or register 1st and then login 
+session_start();
+
 include('config.php');
-include('includes/header.php'); ?>
 
-<div id="wrapper">
-<h1>This is my 1st Website for it261</h1>
-<div id="hero">test hero
-</div> <!-- closes hero -->
+// if user has no logged in correctly will be directed to login page. 
 
-    <main>
-        <h1><?php echo $headline;?></h1><br>
-  
-        <p>I love Halloween. The one time of year when everyone wears a mask … not just me. I am not a killer. I'm a sociopath; there's not much he can do for me. Finding a needle in a haystack isn't hard when every straw is computerized.</p>
-    </main>
-   
-<aside>
-test aside
-</aside>
+if(!isset($_SESSION['username'])) {
+$_SESSION['msg'] = 'Must login first please.';
+header('Location:login.php');
+}
+
+if(isset($_GET['logout'])){
+    session_destroy();
+    unset($_SESSION['username']);
+    header('Location:login.php');
+    
+}
+
+// put header.php include here 
+
+// notification message 
+// if successful, welcome the end user.
+if(isset($_SESSION['success'])) :?>
+<div class="success">
+    <h3>
+<?php echo $_SESSION['success'];
+unset($_SESSION['success']); ?>
+</h3>
+</div>
+<!-- end success div -->
 
 
-<?php
-include('includes/footer.php') ?>
+<?php endif ;
+
+
+if(isset($_SESSION['username'])) : ?>
+<div class="welcome-logout">
+<h3> Well Hello 
+    <?php  echo $_SESSION['username']; ?>
+</h3>
+</div>
+<p><a href="index.php">Log out</a></p>
+</div>
+<!-- end welcome logout div -->
+
+<?php endif ; ?>
+
+<h1>Welcome To the Home Page</h1>
+
+
+<!-- include footer here -->
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+// login page 
+// inout for username and password
+
+include('server.php');
+
+// serve page connected to the config and config connected to the credentials page
+
+// include('header.php');
+
+?>
+
+<h1 class="center">Login</h1>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+
+<fieldset>
+<label for="username">Username</label>
+    <input type="text" name="username" value="<?php if(isset($_POST['username'])) echo $_POST['username']?>">
+
+     <label for="password">Password</label>
+     <input type="password" name="password">
+
+     <button type="submit" name="login_user" class="btn">Login</button>
+
+     <button type="button" onclick="window.location.href='<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ;?>'">Reset</button>
+
+<!--
+include('errors.php'); -->
+  
+     
+</fieldset>
+
+</form>
